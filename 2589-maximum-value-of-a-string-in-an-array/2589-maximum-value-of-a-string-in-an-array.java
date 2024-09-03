af2c0fb1708e42746feb451a6a9d673380667c6c
Pravin Mohite <163111239+KD3-83566-Pravin@users.noreply.github.com>
2024-09-03 16:20:01 +0530

    Time: 1 ms (89.33%), Space: 40.7 MB (91.89%) - LeetHub

--- 2589-maximum-value-of-a-string-in-an-array/2589-maximum-value-of-a-string-in-an-array.java
+++ 2589-maximum-value-of-a-string-in-an-array/2589-maximum-value-of-a-string-in-an-array.java
@@ -1,10 +1,30 @@
 class Solution {
     public int maximumValue(String[] strs) {
-        int ans = 0;
-        for(int i=0;i<strs.length;i++){
-            int num = strs[i].matches("\\d+") ? Integer.parseInt(strs[i]) :strs[i].length();
-            ans = Math.max(ans,num);
+        int ans=0;
+        int value=0;
+        for(String str : strs){
+            int n=check(str);
+            if(n==1){
+                 value=Integer.parseInt(str);
+            }
+            else{
+              value=str.length();
+            }
+             ans = Math.max(ans,value);
         }
-        return ans;
+     return ans;
+        
+    }
+    public int check(String str){
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(Character.isDigit(str.charAt(i))){
+                count++;
+            }
+        }
+        if(count==str.length()){
+            return 1;
+        }
+        return -1;
     }
 }
